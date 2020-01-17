5fd215f0127e81c2b7cb27dd40cf34f51f1fe078
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2020-01-16 23:23:00 -0800

    Results HTML Framework

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World:"
+    return "Hello World"
+
+@app.route('/<zip>')
+def getResults(zip):
+    print(zip)
+    return render_template('results.html')
