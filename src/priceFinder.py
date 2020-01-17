7199c0723adf02336f7a672e721ca8e7fa4ccf8a
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2020-01-16 22:25:30 -0800

    Costco AJAX Parser

---
+++ src/priceFinder.py
@@ -0,0 +1,75 @@
+import os
+import string
+import requests
+import urllib.parse
+
+def getBingResponse(zipCode):
+    '''Get's Bing API Response from a Zip Code Location Query'''
+    apiKey = os.environ['BING_API_KEY']
+    payload = f'http://dev.virtualearth.net/REST/v1/Locations/{zipCode}?maxResults=1&key={apiKey}'
+    return requests.get(payload)
+
+def findCoordinates(bingResponse):
+    '''Returns the Coordinates of a ZIP code from a Bing Response Object'''
+    r = bingResponse.json()
+    return r['resourceSets'][0]['resources'][0]['geocodePoints'][0]['coordinates']
+
+def getCoordinates(ZIP):
+    '''Convience function to return lat and long from a ZIP Code.'''
+    return findCoordinates(getBingResponse(ZIP))
+
+def getCostcoAJAX(coord, maxResponse=10):
+    '''Return the Costco AJAX Response for a pair of coordinates.'''
+    lat = coord[0]
+    lon = coord[1]
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+    payload = f'https://www.costco.com/AjaxWarehouseBrowseLookupView?langId=-1&storeId=10301&numOfWarehouses={maxResponse}&hasGas=false&hasTires=false&hasFood=false&hasHearing=false&hasPharmacy=false&hasOptical=false&hasBusiness=false&hasPhotoCenter=false&tiresCheckout=0&isTransferWarehouse=false&populateWarehouseDetails=true&warehousePickupCheckout=false&latitude={lat}&longitude={lon}&countryCode=US'
+    return requests.get(payload, headers=headers)
+
+class CostcoLocation:
+    def __init__(self, ajax):
+        self.locationID = ajax['locationName']
+        self.streetAddress = string.capwords(ajax['address1'])
+        self.city = string.capwords(ajax['city'])
+        self.state = ajax['state']
+        zipCode = ajax['zipCode'].split('-')
+        self.zip = zipCode[0]
+        try:
+            self.gasPrices(ajax['gasPrices'])
+            self.gasHours(ajax['gasStationHours'])
+        except KeyError:
+            self.gas = False
+        self.formatAddress()
+
+    def gasPrices(self, gasAjax):
+        self.gas = True
+        self.regular = gasAjax['regular'].rstrip('9')
+        self.premium = gasAjax['premium'].rstrip('9')
+
+    def gasHours(self, hoursAjax):
+        self.weekdays = self.formatHours(hoursAjax[0]['time'])
+        self.saturday = self.formatHours(hoursAjax[1]['time'])
+        self.sunday = self.formatHours(hoursAjax[2]['time'])
+    
+    def formatHours(self, hours):
+        time = hours.split('-')
+        return (time[0].rstrip(" "), time[1].lstrip(" "))
+    
+    def formatAddress(self):
+        self.address2 = f'{self.city}, {self.state} {self.zip}'
+        query = f'{self.streetAddress} {self.address2}'
+        urlSafe = urllib.parse.quote(query)
+        self.addressLink = "https://www.google.com/maps/search/?api=1&query=Costco+Gasoline+" + urlSafe
+
+def interpretCostcoAJAX(costcoAJAX):
+    '''Interprets raw Costco AJAX Response and returns useful classes.'''
+    r = costcoAJAX.json()
+    del r[0] # Response is always padded with an irrelevant True statement
+    locations = []
+    for location in r:
+        locations.append(CostcoLocation(location))
+    return locations
+
+def getCostcoLocations(zip):
+    '''Returns list of Costco Location Classes for a Given ZIP Code.'''
+    return interpretCostcoAJAX(getCostcoAJAX(getCoordinates(zip)))
