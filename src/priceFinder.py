70f33ab9de8b51480461f3b623477db596d403a3
Daniel Flanagan <37907774+FlantasticDan@users.noreply.github.com>
2020-01-17 17:58:26 -0800

    Decoy Google Call

--- src/priceFinder.py
+++ src/priceFinder.py
@@ -24,9 +24,9 @@ def getCostcoAJAX(coord, maxResponse=10):
     lon = coord[1]
     print("Sending Request to Costco AJAX")
     s = requests.Session()
-    # print(s.get('http://costco.com'))
     headers = {'referer': 'http://www.costco.com/warehouse-locations', 'Referrer-Policy': 'no-referrer', 'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "0","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
     payload = f'http://www.costco.com/AjaxWarehouseBrowseLookupView?langId=-1&storeId=10301&numOfWarehouses={maxResponse}&hasGas=false&hasTires=false&hasFood=false&hasHearing=false&hasPharmacy=false&hasOptical=false&hasBusiness=false&hasPhotoCenter=false&tiresCheckout=0&isTransferWarehouse=false&populateWarehouseDetails=true&warehousePickupCheckout=false&latitude={lat}&longitude={lon}&countryCode=US'
+    s.get('https://google.com', headers=headers)
     return s.get(payload, headers=headers)
 
 class CostcoLocation:
@@ -66,7 +66,7 @@ class CostcoLocation:
 
 def interpretCostcoAJAX(costcoAJAX):
     '''Interprets raw Costco AJAX Response and returns useful classes.'''
-    print(f'Response from Costco AJAX: {costcoAJAX.text}')
+    print(f'Response from Costco AJAX: {costcoAJAX}')
     r = costcoAJAX.json()
     del r[0] # Response is always padded with an irrelevant True statement
     locations = []
